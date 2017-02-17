44330e6e45363f62078236aa9f408e0e1d9f5579
Francisco Franco <franciscofranco.1990@gmail.com>
2017-02-17 13:58:13 +0100

    display: add a simple api to query the display state (on/off) at any point in time
    Change-Id: Ie6f726ad2ad0d194c46a56526181574b92884c04
    
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -23,6 +23,7 @@
 #include <linux/qpnp/pwm.h>
 #include <linux/err.h>
 #include <linux/string.h>
+#include <linux/display_state.h>
 
 #include "mdss_dsi.h"
 #ifdef TARGET_HW_MDSS_HDMI
@@ -37,8 +38,17 @@
 
 #define VSYNC_DELAY msecs_to_jiffies(17)
 
+<<<<<<< HEAD
 extern char Lcm_name[HARDWARE_MAX_ITEM_LONGTH];
 extern bool is_Lcm_Present;
+=======
+bool display_on = true;
+
+bool is_display_on()
+{
+	return display_on;
+}
+>>>>>>> bdadd05... display: add a simple api to query the display state (on/off) at any point in time
 
 DEFINE_LED_TRIGGER(bl_led_trigger);
 
@@ -877,6 +887,7 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		return -EINVAL;
 	}
 
+    display_on = true;
 	pinfo = &pdata->panel_info;
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
@@ -1032,6 +1043,7 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 		return -EINVAL;
 	}
 
+    display_on = false;
 	pinfo = &pdata->panel_info;
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
